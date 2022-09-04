716ec5653c18a5f48e37c42fa0a93e9afd587e42
Montarex23 <41803365+Montarex23@users.noreply.github.com>
2022-09-04 11:33:19 +0200

    Update main.py

--- Python/Commands/Userinfo/main.py
+++ Python/Commands/Userinfo/main.py
@@ -21,14 +21,14 @@ async def on_message(message):
 				pass
 		else: #Self
 			user = message.author
-		embed = discord.Embed(title=f"Informations about {user}",color=message.author.color)
-		embed.set_thumbnail(url=user.avatar_url)
+		embed = discord.Embed(title=f"Information about {user}",color=message.author.color)
+		embed.set_thumbnail(url=user.avatar.url)
 		embed.add_field(name="ID",value=user.id,inline=False)
 		created = user.created_at.__format__('%A, %d. %B %Y at %H:%M:%S')
 		embed.add_field(name="Account creation",value=created, inline=False)
 		member = message.guild.get_member(user.id)
 		embed.add_field(name="Activity",value=member.activity,inline=False)
-		embed.add_field(name="Avatar",value=f"[URL]({user.avatar_url})",inline=False)
+		embed.add_field(name="Avatar",value=f"[URL]({user.avatar.url})",inline=False)
 		await message.channel.send(embed=embed)
 
 bot.run(token)
